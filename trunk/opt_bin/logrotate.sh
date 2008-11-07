45e0bc9fcb67b68d2de2432453f2fdc7b374d8db
evansmile <evansmile@f3ec03ec-ab0e-11dd-a714-1752b6e4c161>
2008-11-07 10:04:39 +0000

    add opt_packae_main.sh and fix build.sh when run under ../bin_build directory

--- trunk/opt_bin/logrotate.sh
+++ trunk/opt_bin/logrotate.sh
@@ -9,18 +9,6 @@ make_logrotate()
   SRC_HOST=http://ftp.debian.org/debian/pool/main/l/logrotate
   SRC_TAR=$LOGROTATE.orig.tar.gz
   EXE_FILE1=logrotate.exe
-  WORK_D=logrotate
-
-  pushd `pwd`
-
-  if [ -e $TMP_D ]; then
-	cd $TMP_D
-  fi
-  if [ ! -d  $WORK_D ] ; then
-	mkdir -p $WORK_D
-  fi
-
-  cd $WORK_D
 
   if [ ! -e $WEBTAR_D/$SRC_TAR ] ; then
 	wget $SRC_HOST/$SRC_TAR -O $WEBTAR_D/$SRC_TAR
@@ -30,6 +18,7 @@ make_logrotate()
 	wget $SRC_HOST/$DIFF_FILE -O $WEBTAR_D/$DIFF_FILE
   fi
 
+  cd $WORK_D
 
   rm -r -f $SRC_DIR
 
@@ -51,31 +40,21 @@ make_logrotate()
      cp $EXE_FILE1 $BIN_D
   fi
 
-  #
-  popd
 
 }
 
-iam=logrotate.sh
+. ./opt_package_main.sh
+
+package=logrotate
+run_function=make_logrotate
+
+
+iam=$package.sh
 caller=`basename $0`
 echo $iam, $caller
 
-if [ "$caller". == "$iam". ]; then
+WORK_D=$TMP_D/$package
 
-	echo You can\'t run $iam itself.
-	echo Please run \"build $iam\"
 
-	if [ -e build ]; then
-		./build $iam
-	fi
-else
+package_main
 
-	if [ "$OPT_DEV". == "". ]; then
-		echo '<<< $OPT_DEV not defined'
-	        . ./opt.cfg.sh
-		set | grep _D
-	fi
-
-	make_logrotate
-
-fi
---
+++ trunk/opt_bin/opt_package_main.sh
@@ -0,0 +1,38 @@
+
+function package_main()
+{
+   if [ "$caller". == "$iam". ]; then
+
+	echo You can\'t run $iam itself.
+	echo Please run \"build $iam\"
+
+	if [ -e build.sh ]; then
+		./build.sh $iam
+	fi
+   else
+
+	if [ "$OPT_DEV". == "". ]; then
+		echo '<<< $OPT_DEV not defined'
+	        . ./opt.cfg.sh
+		set | grep _D
+	fi
+
+	pushd `pwd`
+
+	if [ ! -d  $WORK_D ] ; then
+		mkdir -p $WORK_D
+	fi
+
+	echo RUN $run_function
+	$run_function
+
+	if [ -e $WORK_D ] && [ "$OPT_CLEAR_WORK_D". == "CLEAR". ] ; then
+		
+		echo CLEAR $WORK_D DIRECTORY
+		rm -rf $WORK_D
+		
+	fi
+	popd
+   fi
+}
+
